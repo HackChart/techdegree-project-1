33aeee9b5be4cbb9a12fb6c2bdf138e895e61ee4
Austin Loos <hackchart@gmail.com>
2020-03-15 20:29:44 -0700

    Update guessing_game.py

--- guessing_game.py
+++ guessing_game.py
@@ -21,6 +21,8 @@ def start_game():
     Welcome to the Number Guessing Game!
     -------------------------------------
     """)
+    if highscore != None:
+        print(f"HIGHSCORE: {highscore}")
     while guess != solution:
         #Validate player guess
         while type(guess) != int: # TODO: RECHECK THIS
@@ -35,17 +37,35 @@ def start_game():
         print(guess)
 
         #Determine guess location
+        tries += 1
         if guess > solution:
             print("It's Lower!")
+            guess = None
         elif guess < solution:
             print("It's Higher!")
+            guess = None
         else:
-            print(f"That's correct! The number was {solution}!")
-        tries += 1
-        guess = None
+            print(f"That's correct! The number was {solution}!\nIt took you {tries} tries to guess the correct number!")
+            if type(highscore) == None or tries < highscore:
+                pass
+
+            accepted_answers = ["y", "n", "yes", "no"]
+            will_play_again = None
+            while will_play_again not in accepted_answers:
+                will_play_again = input("Would you like to play again? (y)es/(n)o: ").lower()
+                if will_play_again in accepted_answers:
+                    if will_play_again == "y":
+                        start_game()
+                    else:
+                        print("Thanks for playing! The game will now exit.")
+                       #print(f"Your HIGHSCORE was {}") #TODO: Format highscore
+                else:
+                    print("Please enter (y)es or (n)o\n")
+        
     
 
 
 if __name__ == '__main__':
     # Kick off the program by calling the start_game function.
+    highscore = None
     start_game()
